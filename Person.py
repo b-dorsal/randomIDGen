26e5f37efd192ce2ccb2e075490f227957f3918e
Brian Dorsey <bdor528@gmail.com>
2015-12-31 13:38:47 -0500

    Functions Expanded
    Person Class functions extended to include birthplace, age inc, job.

--- Person.py
+++ Person.py
@@ -1,15 +1,60 @@
 class Person(object):
+	fName = ""	#first name, required for init
+	lName = ""	#last name, required for init
+	age = 0		#current age, required for init
+	POB = "unknown"	#place of birth, optional
+	job = "unemployed" #current job, optional
+	fullName = ""
 
-	def __init__(self, f, l, age):
+	#Initializer, learn the diff. between this and constructor soon.
+	def __init__(self, f, l, a):
 		self.fName = f
 		self.lName = l
+		self.age = a
+		self.fullName = self.fName, self.lName
 
-
-	def printName():
+	def printName(self):
 		print self.fName, " ", self.lName
+
+	def printAge(self):
+		print self.age
+
+	def addAge(self): #A persons age cannot simply change, this prevents fraud in the Person storage system ;)
+		self.age = self.age + 1
+
+	def updatePOB(self, pob):
+		old = self.POB
+		self.POB = pob
+		print "The POB of ", self.fullName, " has been changed from, ", old, " to, ", self.POB
+
+	def printPOB(self):
+		print self.POB
+		
+	def updateJob(self, job):
+		old = self.job
+		self.job = job
+		print self.fullName, "s job has been changed from, ", old, " to, ", self.job
+
+	def printJob(self):
+		print self.job
+
 
 
 first = Person("Steve", "DingDong", 45)
 
-print "First:", first.fName
-print "Last:", first.lName
+first.printName()	#output name
+first.printAge()	#output age
+
+first.addAge()		#increment age
+first.printAge()	#output age
+
+first.printPOB()	#output birthplace
+first.updatePOB("New York, NY") #change birthplace
+first.printPOB()	#output birthplace
+
+first.printJob()	#output job
+first.updateJob("pilot") #change job
+first.printJob()	#output job
+
+
+
